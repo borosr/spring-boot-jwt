808703e28bcfc15f7defa38c7efabc211d587290
Boros Robert <robbie13.boros@gmail.com>
2019-05-29 22:20:28 +0200

    adding pipeline and showing port

--- src/test/java/hu/flowacademy/authserver/AuthserverApplicationTests.java
+++ src/test/java/hu/flowacademy/authserver/AuthserverApplicationTests.java
@@ -2,15 +2,25 @@ package hu.flowacademy.authserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AuthserverApplicationTests {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@LocalServerPort
+	private int randomServerPort;
 
 	@Test
 	public void contextLoads() {
+		logger.info("Context init ended! Port is: {}", randomServerPort);
 	}
 
 }
